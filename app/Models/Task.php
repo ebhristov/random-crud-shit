5df9f832168648c68aafd3f243ed03bf24843345
Evlogi Hristov <ebhristov@gmail.com>
2025-11-26 22:55:13 +0200

    model and resources

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,12 +11,10 @@ class Task extends Model
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory, SoftDeletes;
 
+    public $timestamps = true;
     protected $fillable = [
         'name',
         'description',
         'completed',
     ];
-
-    public $timestamps = true;
-
 }
