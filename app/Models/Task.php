ce60534c3ce89f53dca23f3da89f3052285bac8c
Evlogi Hristov <ebhristov@gmail.com>
2025-11-26 23:51:46 +0200

    whoops, forgot to make it api

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 final class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    /** @use HasFactory<TaskFactory> */
     use HasFactory, SoftDeletes;
 
     public $timestamps = true;
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('tasks', TaskController::class)->except(['create', 'edit']);
