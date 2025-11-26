353a81c7525016711ca7f0ff31fefabf43180105
Evlogi Hristov <ebhristov@gmail.com>
2025-11-26 22:48:15 +0200

    forgot fields

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'completed',
+    ];
+
+    public $timestamps = true;
+
 }
